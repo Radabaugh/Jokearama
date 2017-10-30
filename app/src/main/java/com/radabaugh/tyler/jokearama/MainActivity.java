595a1112a1c218ef48963a8579249da7f8fb069e
Tyler Radabaugh <radabaughtyler24@gmail.com>
2017-10-30 11:44:10 -0700

    Added fragment manager

--- app/src/main/java/com/radabaugh/tyler/jokearama/MainActivity.java
+++ app/src/main/java/com/radabaugh/tyler/jokearama/MainActivity.java
@@ -10,6 +10,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new JokeFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
     }
 }
