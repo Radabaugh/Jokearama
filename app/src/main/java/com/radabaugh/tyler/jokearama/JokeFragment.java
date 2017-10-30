7197c9b8e40df0dd17a80c99ec18c8f10d49a9c3
Tyler Radabaugh <radabaughtyler24@gmail.com>
2017-10-30 11:08:57 -0700

    Added layout inflater to JokeFragment

--- app/src/main/java/com/radabaugh/tyler/jokearama/JokeFragment.java
+++ app/src/main/java/com/radabaugh/tyler/jokearama/JokeFragment.java
@@ -2,6 +2,9 @@ package com.radabaugh.tyler.jokearama;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Tyler Radabaugh on 10/30/2017.
@@ -14,5 +17,11 @@ public class JokeFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mJoke = new Joke();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_joke, container, false);
+        return v;
     }
 }
