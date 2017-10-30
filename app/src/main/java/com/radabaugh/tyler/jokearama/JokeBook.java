31459d3b23618d7916ed7e931ccf8e7b20294416
Tyler Radabaugh <radabaughtyler24@gmail.com>
2017-10-30 11:35:57 -0700

    Create new jokes

--- app/src/main/java/com/radabaugh/tyler/jokearama/JokeBook.java
+++ app/src/main/java/com/radabaugh/tyler/jokearama/JokeBook.java
@@ -2,12 +2,18 @@ package com.radabaugh.tyler.jokearama;
 
 import android.content.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 /**
  * Created by Tyler Radabaugh on 10/30/2017.
  */
 
 public class JokeBook {
     private static JokeBook sJokeBook;
+
+    private List<Joke> mJokes;
 
     public static JokeBook get(Context context) {
         if (sJokeBook == null) {
@@ -17,6 +23,25 @@ public class JokeBook {
     }
 
     private JokeBook(Context context) {
+        mJokes = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            Joke joke = new Joke();
+            joke.setTitle("Joke " + i);
+            joke.setViewed(false);
+            mJokes.add(joke);
+        }
+    }
 
+    public List<Joke> getJokes() {
+        return mJokes;
+    }
+
+    public Joke getJoke(UUID id) {
+        for (Joke joke : mJokes) {
+            if (joke.getId().equals(id)) {
+                return joke;
+            }
+        }
+        return null;
     }
 }
